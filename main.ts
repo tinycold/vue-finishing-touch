d5c2eb8505a7c2573d72d7d9a19a76cdfcf64780
Xubo Leng <lengxubo@linctex.com>
2021-11-05 10:57:44 +0800

    feat: init project

---
+++ main.ts
@@ -0,0 +1,10 @@
+const fs = require('fs')
+const path = require('path')
+
+// read file
+// compile => descriptor
+// convert descriptor.script to ast
+// traverse ast
+// generate code
+// compose descriptor
+// write file
